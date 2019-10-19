6c7852ac7f24088e08221fdd54a4c067d4eddcc0
Hung <hungpham2511@gmail.com>
2019-10-19 22:27:57 +0800

    firmware allows higher reading rate

--- photosensor_biopac/main/main.ino
+++ photosensor_biopac/main/main.ino
@@ -1,40 +1,70 @@
-#include <RBD_Timer.h>
-
 // the setup routine runs once when you press reset:
-int index;
-unsigned long cur_time, prev_time;
+int index, sensorValue;
+unsigned long cur_time, prev_time, diff_time, deadline;
 unsigned long duration, max_duration, min_duration;
+unsigned int period_usec, hz, threshold;
 
-RBD::Timer timer;
- 
 
 // setup routine
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  hz = 2500;
+
+  // calibrate and change threshold according to the experiment
+  threshold = 100;
+
+  period_usec = 1000000 / hz;
+  Serial.println(period_usec);
+
   pinMode(2, OUTPUT);
   pinMode(13, OUTPUT);
-  timer.setTimeout(100);
-  timer.restart();
+
+  max_duration = 0;
+  min_duration = 1000000;
+  deadline = micros();
 }
 
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  if(timer.onRestart()) {
-    int sensorValue = analogRead(A0);
-    /* Serial.println("Analog read: "); */
-    /* Serial.println(sensorValue); */
-    
-    if (sensorValue < 100) {
+  cur_time = micros();
+
+  // todo: handle rollover
+  if(deadline < cur_time) {
+    deadline = deadline + period_usec;
+   
+    // main work
+    sensorValue = analogRead(A0);
+    if (sensorValue < threshold) {
       digitalWrite(2, LOW);
       digitalWrite(13, HIGH);
     }
     else {
       digitalWrite(2, HIGH);
-      /* digitalWrite(13, HIGH); */
       digitalWrite(13, LOW);
     }
+
+    // report and inspection
+    index = (index + 1) % (3 * hz);
+    diff_time = cur_time - prev_time;
+    prev_time = cur_time;
+    if (diff_time > max_duration) {
+      max_duration = diff_time;
+    } else if (diff_time < min_duration) {
+      min_duration = diff_time;
+    }
+
+    if (index == 0) {
+      /* Serial.println(index); */
+      /* Serial.println(max_duration); */
+      /* Serial.println(min_duration); */
+      Serial.println("Analog read: ");
+      Serial.println(sensorValue);
+      /* Serial.println(cur_time); */
+      max_duration = 0;
+      min_duration = 1000000;
+      }
+
   }	
 }
